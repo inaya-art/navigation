75fd23607014228b2f56c272336477cb55ea263f
Inaya Dar <inayadar@gmail.com>
2024-06-24 08:38:12 -0500

    Initial Commit

---
+++ navigation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  navigation
+//
+//  Created by Inaya Dar on 6/24/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ navigation/navigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  navigationApp.swift
+//  navigation
+//
+//  Created by Inaya Dar on 6/24/24.
+//
+
+import SwiftUI
+
+@main
+struct navigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
